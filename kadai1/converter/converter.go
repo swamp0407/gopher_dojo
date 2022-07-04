87af94d89b0f3d755b0b523585c682989d1827ab
Tatsuya <swamp0407@gmail.com>
2022-07-04 23:03:02 +0900

    split files and add main.go

--- kadai1/converter/converter.go
+++ kadai1/converter/converter.go
@@ -1,13 +1,14 @@
 package converter
 
 import (
+	"fmt"
 	"image"
 	"image/jpeg"
 	"image/png"
+
 	"io"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 func image2png(img image.Image, savefile io.Writer) error {
@@ -15,32 +16,57 @@ func image2png(img image.Image, savefile io.Writer) error {
 	return png
 }
 
-func Convert() error {
-	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+func image2jpg(img image.Image, savefile io.Writer) error {
+	jpg := jpeg.Encode(savefile, img, &jpeg.Options{Quality: 100})
+	return jpg
+}
+
+func replaceExt(filePath, from, to string) string {
+	ext := filepath.Ext(filePath)
+	if len(from) > 0 && ext != "."+from {
+		return filePath
+	}
+	return filePath[:len(filePath)-len(ext)] + "." + to
+}
+
+func Convert(dir, ext_from, ext_to string) error {
+	fmt.Println("Converting...")
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if info.IsDir() {
 			return nil
 		}
-		if filepath.Ext(path) == ".jpg" || filepath.Ext(path) == ".jpeg" {
-			j, err := os.Open(path)
+		if filepath.Ext(path) == "."+ext_from {
+			fmt.Printf("%s\n", path)
+
+			f, err := os.Open(path)
 			if err != nil {
 				return err
 			}
-			defer j.Close()
-			img, err := jpeg.Decode(j)
+			defer f.Close()
+			img, _, err := image.Decode(f)
 			if err != nil {
 				return err
 			}
-			outf := filepath.Join(filepath.Dir(path), strings.TrimRight(filepath.Base(path), ".jpg")+".png")
-			savefile, err := os.Create(outf)
+			dstfileName := replaceExt(filepath.Base(path), ext_from, ext_to)
+			dstfile, err := os.Create(filepath.Join("output", dstfileName))
+
 			if err != nil {
 				return err
 			}
-			err = image2png(img, savefile)
-			if err != nil {
-				return err
+			defer dstfile.Close()
+
+			if ext_to == "png" {
+				if err := image2png(img, dstfile); err != nil {
+					return err
+				}
+			}
+			if ext_to == "jpg" || ext_to == "jpeg" {
+				if err := image2jpg(img, dstfile); err != nil {
+					return err
+				}
 			}
 		}
 		return nil
